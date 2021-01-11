44a2c558d50d265a3b057ac6ca12588d635b578d
Nosjazz <vandendriessche.n@hotmail.be>
2021-01-11 10:06:42 +0000

    add javascript for emailjs

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,18 @@ $(document).ready(function () {
     $('textarea#message').characterCounter();
     $('.collapsible').collapsible();
 });
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "template_ds0eavp", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+}
